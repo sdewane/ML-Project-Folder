868498ce38537b9c26322a81da9bf7960d1b2fab
sdewane <sdewane112@gmail.com>
2026-04-04 18:11:00 -0600

    put the data in a seperate folder called data and added basic configurations

--- model.py
+++ model.py
@@ -18,7 +18,32 @@ from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import (
     mean_absolute_error,
     root_mean_squared_error,
-    r2_score,
+    r2_score
 )
 
 print("All libraries imported successfully.")   
+
+#config
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: f"{x:.3f}")
+sns.set_theme(style="darkgrid")
+
+plt.rcParams.update({
+    "axes.titlesize": 10,
+    "axes.labelsize": 9,
+    "xtick.labelsize": 8,
+    "ytick.labelsize": 8
+})
+
+RANDOM_STATE = 42
+CSV_PATH = "data/housing.csv" #change depending on dataset
+TARGET_COL= "median_house_value" #target variable for prediction
+
+#load data
+data = pd.read_csv(CSV_PATH)
+print("Data loaded successfully.")
+#display basic info about the dataset
+print("Dataset shape:", data.shape)
+print("Dataset columns:", data.columns.tolist())
+
+data.head()
